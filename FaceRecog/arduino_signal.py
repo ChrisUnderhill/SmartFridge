05fef03a67fad16e2ef38b62af0024332fd691ef
smjcrane <smjcrane@gmail.com>
2020-01-18 22:04:02 +0000

    picked up milk

--- FaceRecog/arduino_signal.py
+++ FaceRecog/arduino_signal.py
@@ -1,4 +1,4 @@
-/usr/bin/python3
+#!/usr/bin/python3
 
 import serial, string, time, os, quickstart
 from serial import Serial
@@ -7,13 +7,26 @@ from serial import Serial
 output = " "
 ser = serial.Serial('/dev/ttyACM0', 9600, 8, 'N', 1, timeout=1);
 
+def on_picked_up():
+    print("You picked something up")
+
+def on_put_down():
+    print("You put something down")
+
+old_pressure = 0
 while True:
     temp = ser.readline();
-    if(temp  == b''):
-        print("Off")
-    else:
+    s = temp.decode("utf-8").strip()
+    touch, pressure = s.split(",")
+    new_pressure = int(pressure)
+    if touch != "0":
+        # take a picture
         timestr = time.strftime("%H%M%S-%d%m%Y")
         os.system("fswebcam -r 1280x720 " + timestr + ".jpg")
         print(quickstart.who_is_it(timestr + ".jpg"))
         ser.reset_input_buffer()
         time.sleep(5)
+    if new_pressure > old_pressure:
+        on_put_down()
+    elif new_pressure < old_pressure:
+        on_picked_up()
