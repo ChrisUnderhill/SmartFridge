0f14480073636f8950de120d0e2c2ebebb945aa6
smjcrane <smjcrane@gmail.com>
2020-01-18 22:05:31 +0000

    debug

--- FaceRecog/arduino_signal.py
+++ FaceRecog/arduino_signal.py
@@ -19,13 +19,15 @@ while True:
     s = temp.decode("utf-8").strip()
     touch, pressure = s.split(",")
     new_pressure = int(pressure)
-    if touch != "0":
-        # take a picture
-        timestr = time.strftime("%H%M%S-%d%m%Y")
-        os.system("fswebcam -r 1280x720 " + timestr + ".jpg")
-        print(quickstart.who_is_it(timestr + ".jpg"))
-        ser.reset_input_buffer()
-        time.sleep(5)
+    print("touch", touch)
+    print("pressure", pressure)
+    # if touch != "0":
+    #     # take a picture
+    #     timestr = time.strftime("%H%M%S-%d%m%Y")
+    #     os.system("fswebcam -r 1280x720 " + timestr + ".jpg")
+    #     print(quickstart.who_is_it(timestr + ".jpg"))
+    #     ser.reset_input_buffer()
+    #     time.sleep(5)
     if new_pressure > old_pressure:
         on_put_down()
     elif new_pressure < old_pressure:
