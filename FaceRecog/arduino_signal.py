9dbe5f64b8fb845ba18196c80cea7ed1ee444125
apseros <apseros.andreas@gmail.com>
2020-01-18 18:16:05 +0000

    Added the raspberry pi script

---
+++ FaceRecog/arduino_signal.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import serial, string, time, os, quickstart
+from serial import Serial
+
+
+output = " "
+ser = serial.Serial('/dev/ttyACM0', 9600, 8, 'N', 1, timeout=1);
+
+while True:
+    temp = ser.readline();
+    if(temp  == b''):
+        print("Off")
+    else:
+        print(temp)
+        timestr = time.strftime("%H%M%S-%d%m%Y")
+        os.system("fswebcam -r 1280x720 " + timestr + ".jpg")
+        print(quickstart.who_is_it(timestr + ".jpg"))
+        ser.reset_input_buffer()
+        time.sleep(5)
